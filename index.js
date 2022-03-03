670c73b7caf64d84f725062e4728b420d56f8d10
XXLZopes <afordm99@gmail.com>
2022-03-02 20:24:19 -0800

    looks good on phone now

--- index.js
+++ index.js
@@ -1,24 +1,56 @@
 const burgerMenuEl = document.querySelector(".burger-menu");
 const mobileNavEl = document.querySelector(".mobile-screen-nav");
+const line1El = document.querySelector("#line1");
+const line2El = document.querySelector("#line2");
+const line3El = document.querySelector("#line3");
 const bodyEl = document.querySelector("body");
 
 let isMobileNavOpen = false;
 
 burgerMenuOnclick = ()=> { 
     !isMobileNavOpen ? isMobileNavOpen=true : isMobileNavOpen=false;
-    isMobileNavOpen ? mobileNavEl.style.display="block" : mobileNavEl.style.display="none";
+    isMobileNavOpen ? (
+        mobileNavEl.style.display="block",
+        line1El.style.background="#39a6b2",
+        line2El.style.background="#39a6b2",
+        line3El.style.background="#39a6b2"
+        ) : 
+        (
+            mobileNavEl.style.display="none",
+            line1El.style.background="#fce138",
+            line2El.style.background="#fce138",
+            line3El.style.background="#fce138"
+        );
+    
 }
 
 document.addEventListener('click', function(e) {
     e = e || window.event;
-    let target = e.srcElement.className;
-        console.log(target);
-        if (target=="burger-menu" || target=="line") {
+    let targeted = e.target.className;
+    console.log(targeted);
+        if (targeted=="burger-menu" || targeted=="line") {
             burgerMenuOnclick();
         } else {
             isMobileNavOpen=false;
-            mobileNavEl.style.display="none"
-            console.log("hi")
+            mobileNavEl.style.display="none";
+            line1El.style.background="#fce138";
+            line2El.style.background="#fce138";
+            line3El.style.background="#fce138";
         }
+});
+
+window.addEventListener('resize', ()=> {
+    if (window.innerWidth >= 768) {
+        burgerMenuEl.style.display="none";
+        mobileNavEl.style.display="none";
+        isMobileNavOpen=false;
+    } else {
+        burgerMenuEl.style.display="flex";
+    }
+    if (!isMobileNavOpen) {
+        line1El.style.background="#fce138";
+        line2El.style.background="#fce138";
+        line3El.style.background="#fce138";
+    }
 });
 
